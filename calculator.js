172269e4e30a63fff8b16546455c9e274381d5c0
karlGusta <karlgustaesimit@gmail.com>
2020-11-19 14:18:30 +0300

    Added the input disabled and updateDisplay fctn

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// This is a calculator object that consists of everything that
+// we need to construct a valid expression
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+
+function updateDisplay() {
+  // Select the element with class of `calculator-screen`
+  const display = document.querySelector(".calculator-screen");
+  // Update the value of the element with the contents of `displayValue`
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+
